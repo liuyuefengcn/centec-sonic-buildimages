446b6bd8d6ab8137caefb3eaea4df2f524b6a6cc
Xianghong Gu <xgu@centecnetworks.com>
2021-03-15 09:41:32 +0000

    [centec][arm64]use CONFIGURED_ARCH for syncd debian package

--- rules/syncd.mk
+++ rules/syncd.mk
@@ -2,7 +2,7 @@
 
 ifneq ($(CONFIGURED_PLATFORM),vs)
 
-SYNCD = syncd_1.0.0_amd64.deb
+SYNCD = syncd_1.0.0_$(CONFIGURED_ARCH).deb
 $(SYNCD)_RDEPENDS += $(LIBSAIREDIS) $(LIBSAIMETADATA)
 $(SYNCD)_DPKG_TARGET = binary-syncd
 $(SYNCD)_SRC_PATH = $(SRC_PATH)/sonic-sairedis
@@ -12,7 +12,7 @@ $(SYNCD)_DEB_BUILD_OPTIONS = nocheck
 SONIC_DPKG_DEBS += $(SYNCD)
 
 ifeq ($(ENABLE_SYNCD_RPC),y)
-SYNCD_RPC = syncd-rpc_1.0.0_amd64.deb
+SYNCD_RPC = syncd-rpc_1.0.0_$(CONFIGURED_ARCH).deb
 $(SYNCD_RPC)_RDEPENDS += $(LIBSAIREDIS) $(LIBSAIMETADATA)
 $(eval $(call add_derived_package,$(SYNCD),$(SYNCD_RPC)))
 
@@ -21,13 +21,13 @@ $(SYNCD)_DEPENDS += $(LIBSWSSCOMMON_DEV) $(LIBTHRIFT_DEV)
 $(SYNCD)_DPKG_TARGET = binary-syncd-rpc
 endif
 
-SYNCD_DBG = syncd-dbg_1.0.0_amd64.deb
+SYNCD_DBG = syncd-dbg_1.0.0_$(CONFIGURED_ARCH).deb
 $(SYNCD_DBG)_DEPENDS += $(SYNCD)
 $(SYNCD_DBG)_RDEPENDS += $(SYNCD)
 $(eval $(call add_derived_package,$(SYNCD),$(SYNCD_DBG)))
 
 ifeq ($(ENABLE_SYNCD_RPC),y)
-SYNCD_RPC_DBG = syncd-rpc-dbg_1.0.0_amd64.deb
+SYNCD_RPC_DBG = syncd-rpc-dbg_1.0.0_$(CONFIGURED_ARCH).deb
 $(SYNCD_RPC_DBG)_DEPENDS += $(SYNCD_RPC)
 $(SYNCD_RPC_DBG)_RDEPENDS += $(SYNCD_RPC)
 $(eval $(call add_derived_package,$(SYNCD),$(SYNCD_RPC_DBG)))
