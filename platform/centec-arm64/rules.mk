4cbfee2badc32ce698cec1448bcc055d98ad5d45
Xianghong Gu <xgu@centecnetworks.com>
2021-03-15 09:32:26 +0000

    Merge PR #6467. [docker-orchagent]: make build depends only on sairedis package

--- platform/centec-arm64/rules.mk
+++ platform/centec-arm64/rules.mk
@@ -11,8 +11,9 @@ SONIC_ALL += $(SONIC_ONE_IMAGE) \
              $(DOCKER_FPM)
 #             $(DOCKER_SYNCD_CENTEC_RPC)
 
-# Inject centec sai into sairedis
-$(LIBSAIREDIS)_DEPENDS += $(CENTEC_SAI) $(LIBSAITHRIFT_DEV_CENTEC)
+# Inject centec sai into syncd
+$(SYNCD)_DEPENDS += $(CENTEC_SAI)
+$(SYNCD)_UNINSTALLS += $(CENTEC_SAI)
 
 # Runtime dependency on centec sai is set only for syncd
 $(SYNCD)_RDEPENDS += $(CENTEC_SAI)
