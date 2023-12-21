a483709273446f8b44e6c3b322345badfd0e641d
qianj <qianj@centec.com>
2023-12-21 11:14:03 +0800

    [centec][arm64] support g530-sr100-8t4s26x

--- platform/centec-arm64/sai.mk
+++ platform/centec-arm64/sai.mk
@@ -1,7 +1,7 @@
 # Centec SAI
 
-export CENTEC_SAI_VERSION = 1.7.1-4
-export CENTEC_SAI = libsai_$(CENTEC_SAI_VERSION)_$(PLATFORM_ARCH).deb
+export CENTEC_SAI_VERSION = 1.7.1-6
+export CENTEC_SAI = libsai_$(CENTEC_SAI_VERSION)_$(PLATFORM_ARCH)_custom_xy.deb
 
 $(CENTEC_SAI)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/sai/$(CENTEC_SAI)
 $(eval $(call add_conflict_package,$(CENTEC_SAI),$(LIBSAIVS_DEV)))
